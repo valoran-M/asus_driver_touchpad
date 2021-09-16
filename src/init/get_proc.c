75a3f298b22b1464b2e5c59f800fb609a5f580f2
valoran-M <contact-me@valeran-maytie.com>
2021-09-16 14:18:50 +0200

    remove binary and change include

--- src/init/get_proc.c
+++ src/init/get_proc.c
@@ -5,13 +5,14 @@
 
 #define DEVICES_LISTE_PATH "/proc/bus/input/devices"
 
-devices_info* get_devices()
+devices_info *get_devices()
 {
     devices_info *info = malloc(sizeof(devices_info));
 
     FILE *devices = fopen(DEVICES_LISTE_PATH, "r");
 
     fclose(devices);
-    
-    return info;
+    free(info);
+
+    return 0;
 }
