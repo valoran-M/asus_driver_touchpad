8a27e1be10a7cd5d8ac3794a4ca442ba1ebed7ae
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2021-10-12 20:48:17 +0200

    Fix read incorrect behavior

--- src/event/run.c
+++ src/event/run.c
@@ -2,10 +2,13 @@
 #include <unistd.h>
 #include <math.h>
 #include <linux/input.h>
+#include <sys/poll.h>
 
 #include "event.h"
 #include "material.h"
 #include "utility/utility.h"
+#include "run.h"
+
 
 int running = 1;
 
@@ -24,7 +27,7 @@ void run(const devices_info *dev_info)
     } position;
     int numlock = 0, finger = 0;
     int col = 0, line = 0;
-    key value = (key){-1, -1};
+    key value = (key) {-1, -1};
     position.x = 0;
     position.y = 0;
 
@@ -32,74 +35,66 @@ void run(const devices_info *dev_info)
 
     emit(dev_info, EV_KEY, KEY_NUMLOCK, 0);
 
-    while (running)
-    {
-        read(dev_info->file_touchpad, &event, sizeof(event));
+    while (running) {
 
-        if (event.type != EV_ABS &&
-            event.type != EV_KEY &&
-            event.code != ABS_MT_POSITION_X &&
-            event.code != ABS_MT_POSITION_Y &&
-            event.code != BTN_TOOL_FINGER)
-            continue;
-
-        if (event.type == EV_ABS && (event.code == ABS_MT_POSITION_X ||
-                                     event.code == ABS_MT_POSITION_Y))
-        {
-            if (event.code == ABS_MT_POSITION_X)
-                position.x = event.value;
-            else if (event.code == ABS_MT_POSITION_Y)
-                position.y = event.value;
+        if (ready_to_read(dev_info->file_touchpad)) {
+            read(dev_info->file_touchpad, &event, sizeof(event));
+        }else {
             continue;
         }
 
-        if (event.code == BTN_TOOL_FINGER)
-        {
-            if (event.value == 0)
-            {
+        if (event.type == EV_ABS) {
+            if (event.code == ABS_MT_POSITION_X) {
+                position.x = event.value;
+            }
+            else if (event.code == ABS_MT_POSITION_Y) {
+                position.y = event.value;
+            }
+            continue;
+        }
+
+        if (event.code == BTN_TOOL_FINGER) {
+            if (event.value == 0) {
                 finger = 0;
-                if (value.shifted != -1)
-                {
+                if (value.shifted != -1) {
                     remove_key(dev_info, value);
-                    value = (key){-1, -1};
+                    value = (key) {-1, -1};
                 }
             }
-            if (event.value == 1 && finger == 0)
+            if (event.value == 1 && finger == 0) {
                 finger = 1;
+            }
 
             if (event.value == 1 &&
-                (position.x > 0.95 * (double)dev_info->x.max &&
-                 position.y < 0.09 * (double)dev_info->y.max))
-            {
+                (position.x > 0.95 * (double) dev_info->x.max &&
+                 position.y < 0.09 * (double) dev_info->y.max)) {
                 finger = 0;
                 numlock = !numlock;
-                if (numlock)
+                if (numlock) {
                     activate_numpad(dev_info);
-                else
+                }
+                else {
                     deactivate_numpad(dev_info);
+                }
             }
             else if (event.value == 1 &&
-                     (position.x < 0.06 * (double)dev_info->x.max &&
-                      position.y < 0.07 * (double)dev_info->y.max))
-            {
+                     (position.x < 0.06 * (double) dev_info->x.max &&
+                      position.y < 0.07 * (double) dev_info->y.max)) {
                 finger = 0;
                 change_brightness(dev_info);
             }
 
-            if (numlock)
-            {
-                if (finger == 1)
-                {
+            if (numlock) {
+                if (finger == 1) {
                     finger = 2;
 
-                    col = floor((double)dev_info->colonne * (double)position.x /
-                                (double)dev_info->x.max);
-                    line = floor(((double)dev_info->line * (double)position.y /
-                                  (double)dev_info->y.max) -
+                    col = floor((double) dev_info->colonne * (double) position.x /
+                                (double) dev_info->x.max);
+                    line = floor(((double) dev_info->line * (double) position.y /
+                                  (double) dev_info->y.max) -
                                  0.3);
 
-                    if (line >= 0)
-                    {
+                    if (line >= 0) {
                         value = dev_info->keys[line][col];
                         emit_key(dev_info, value);
                     }
@@ -107,4 +102,17 @@ void run(const devices_info *dev_info)
             }
         }
     }
+}
+
+int ready_to_read(int fd)
+{
+    static struct pollfd fds;
+    fds.fd = fd;
+    fds.events = POLLIN;
+
+    if (poll(&fds, 1, 1000) > 0) {  // 1000 ms : timeout
+        return fds.revents & POLLIN;
+    }
+
+    return 0;
 }
--- src/event/run.h
+++ src/event/run.h
@@ -1,12 +1,16 @@
 #ifndef RUN_H
 #define RUN_H
 
+#include <stdbool.h>
 #include "../utility/utility.h"
+
 /**
  * 
  * run driver
  * 
  **/
 void run(const devices_info *dev_info);
+
+int ready_to_read(int fd);
 
 #endif
