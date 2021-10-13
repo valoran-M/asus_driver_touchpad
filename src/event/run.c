e998cfd97f45948ab07d7f975c8d17079094270c
valomay <valomay@protonmail.com>
2021-10-13 10:14:43 +0200

    clean { }

--- src/event/run.c
+++ src/event/run.c
@@ -9,7 +9,6 @@
 #include "utility/utility.h"
 #include "run.h"
 
-
 int running = 1;
 
 void sighandler(int sig)
@@ -27,7 +26,7 @@ void run(const devices_info *dev_info)
     } position;
     int numlock = 0, finger = 0;
     int col = 0, line = 0;
-    key value = (key) {-1, -1};
+    key value = (key){-1, -1};
     position.x = 0;
     position.y = 0;
 
@@ -35,66 +34,70 @@ void run(const devices_info *dev_info)
 
     emit(dev_info, EV_KEY, KEY_NUMLOCK, 0);
 
-    while (running) {
+    while (running)
+    {
 
-        if (ready_to_read(dev_info->file_touchpad)) {
+        if (ready_to_read(dev_info->file_touchpad))
             read(dev_info->file_touchpad, &event, sizeof(event));
-        }else {
+        else
             continue;
-        }
 
-        if (event.type == EV_ABS) {
-            if (event.code == ABS_MT_POSITION_X) {
+        if (event.type == EV_ABS)
+        {
+            if (event.code == ABS_MT_POSITION_X)
                 position.x = event.value;
-            }
-            else if (event.code == ABS_MT_POSITION_Y) {
+            else if (event.code == ABS_MT_POSITION_Y)
                 position.y = event.value;
-            }
             continue;
         }
 
-        if (event.code == BTN_TOOL_FINGER) {
-            if (event.value == 0) {
+        if (event.code == BTN_TOOL_FINGER)
+        {
+            if (event.value == 0)
+            {
                 finger = 0;
-                if (value.shifted != -1) {
+                if (value.shifted != -1)
+                {
                     remove_key(dev_info, value);
-                    value = (key) {-1, -1};
+                    value = (key){-1, -1};
                 }
             }
-            if (event.value == 1 && finger == 0) {
+            if (event.value == 1 && finger == 0)
                 finger = 1;
-            }
 
             if (event.value == 1 &&
-                (position.x > 0.95 * (double) dev_info->x.max &&
-                 position.y < 0.09 * (double) dev_info->y.max)) {
+                (position.x > 0.95 * (double)dev_info->x.max &&
+                 position.y < 0.09 * (double)dev_info->y.max))
+            {
                 finger = 0;
                 numlock = !numlock;
-                if (numlock) {
+                if (numlock)
                     activate_numpad(dev_info);
-                }
-                else {
+                else
                     deactivate_numpad(dev_info);
-                }
             }
             else if (event.value == 1 &&
-                     (position.x < 0.06 * (double) dev_info->x.max &&
-                      position.y < 0.07 * (double) dev_info->y.max)) {
+                     (position.x < 0.06 * (double)dev_info->x.max &&
+                      position.y < 0.07 * (double)dev_info->y.max))
+            {
                 finger = 0;
                 change_brightness(dev_info);
             }
 
-            if (numlock) {
-                if (finger == 1) {
+            if (numlock)
+            {
+                if (finger == 1)
+                {
                     finger = 2;
 
-                    col = floor((double) dev_info->colonne * (double) position.x /
-                                (double) dev_info->x.max);
-                    line = floor(((double) dev_info->line * (double) position.y /
-                                  (double) dev_info->y.max) -
+                    col = floor((double)dev_info->colonne * (double)position.x /
+                                (double)dev_info->x.max);
+                    line = floor(((double)dev_info->line * (double)position.y /
+                                  (double)dev_info->y.max) -
                                  0.3);
 
-                    if (line >= 0) {
+                    if (line >= 0)
+                    {
                         value = dev_info->keys[line][col];
                         emit_key(dev_info, value);
                     }
@@ -110,7 +113,8 @@ int ready_to_read(int fd)
     fds.fd = fd;
     fds.events = POLLIN;
 
-    if (poll(&fds, 1, 1000) > 0) {  // 1000 ms : timeout
+    if (poll(&fds, 1, 1000) > 0)
+    { // 1000 ms : timeout
         return fds.revents & POLLIN;
     }
 
